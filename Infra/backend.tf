e43e34acbae2c028f3b376142c5dd05fc6d3fe8a
khushi agrahari <khushiagrahari05@gmail.com>
2025-06-12 16:46:02 +0530

    pipeline fix

--- Infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "rg-khushi-tfstate-01"
-    storage_account_name  = "sakhushitfstate01"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
