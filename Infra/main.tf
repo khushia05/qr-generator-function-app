70f9f86f7088e1f01c17c2b9fa6a6bc88ae8214f
khushi agrahari <khushiagrahari05@gmail.com>
2025-06-12 16:56:08 +0530

    pipeline fix

--- Infra/main.tf
+++ Infra/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_service_plan" "plan" {
 }
 
 resource "azurerm_application_insights" "app_insights" {
-  name                = "my-function-insights"
+  name                = var.application_insight_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
